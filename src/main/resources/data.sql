41c0a0101fb1b584bc0e187905bca0b53e30ba5f
SubinC <SubinC@mashreq.com>
2020-02-10 11:31:26 +0400

    Changes in master.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO SWAGGER_SPEC VALUES
+(1, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(2, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(3, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(4, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(5, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(6, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(7, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(8, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(9, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(10, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(11, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(12, 'Sample','1','https://your-domain/api/v2/api-docs','2.0'),
+(13, 'Sample','1','https://your-domain/api/v2/api-docs','2.0');
